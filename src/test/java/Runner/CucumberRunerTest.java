bddd17754fe2a5ea8b3b646de891e2ed2b9ca483
ngracian <ngracian@grupobmv.com.mx>
2023-08-09 11:18:05 -0600

    feat(web): agregar Runer para la ejecucion de las pruebas con cucumber
    Se hace la relacion al feature y a los step definitions con cucumber y se hace anotacion para utilizar junit

---
+++ src/test/java/Runner/CucumberRunerTest.java
@@ -0,0 +1,15 @@
+package Runner;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/Features"
+        ,glue={"StepsDefinitions"}
+)
+public class CucumberRunerTest {
+
+}
