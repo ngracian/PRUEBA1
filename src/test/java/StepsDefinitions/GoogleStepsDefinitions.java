f2c43e6976f3b6b8ffb7f0646bbf2a782814f007
ngracian <ngracian@grupobmv.com.mx>
2023-08-09 11:15:37 -0600

    feat(web): agregar Steps Definitions para escenario de busqueda en google
    Se describen los pasos a seguir.

---
+++ src/test/java/StepsDefinitions/GoogleStepsDefinitions.java
@@ -0,0 +1,37 @@
+package StepsDefinitions;
+
+import io.cucumber.java.en.*;
+
+public class GoogleStepsDefinitions {
+    @Given("^Dado que el usuario abre el navegador$")
+    public void dado_que_el_usuario_abre_el_navegador() {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+    @When("^Carga el sitio web$")
+    public void carga_el_sitio_web() {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+    @Then("^El usuario da clic en la barra de busqueda$")
+    public void el_usuario_da_clic_en_la_barra_de_busqueda() {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+    @Given("^Dado ingresa la consulta de la sintaxis gherkin$")
+    public void dado_ingresa_la_consulta_de_la_sintaxis_gherkin() {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+    @When("^El usuario da clic en el boton de Buscar$")
+    public void el_usuario_da_clic_en_el_boton_de_buscar() {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+    @Then("^El navegador despliega los resultados$")
+    public void el_navegador_despliega_los_resultados() {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+
+}
