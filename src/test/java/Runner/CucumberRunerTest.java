50f6beb1a74415fd0c76a9352cb69a77c4f0a034
ngracian <ngracian@grupobmv.com.mx>
2023-08-16 12:26:45 -0600

    feat(web): agregar anotaciones Before y After de junit

--- src/test/java/Runner/CucumberRunerTest.java
+++ src/test/java/Runner/CucumberRunerTest.java
@@ -1,8 +1,11 @@
 package Runner;
 
 
+import Pages.BasePage;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
@@ -11,5 +14,13 @@ import org.junit.runner.RunWith;
         ,glue={"StepsDefinitions"}
 )
 public class CucumberRunerTest {
+    @Before
+    public static void CrearDriver(){
+        BasePage.abrirNavegador();
+    }
+    @After
+    public static void EliminarDriver(){
+        BasePage.cerrarNavegador();
+    }
 
 }
